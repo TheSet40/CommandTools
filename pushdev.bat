fd33304761309eeeb7ba21150a83f8354271922d
Jonatan Näslund <jonatan.naslund@sherpas.se>
2023-11-30 11:03:46 +0100

    push för dev branch

--- pushdev.bat
+++ pushdev.bat
@@ -1,20 +1,4 @@
-@echo off 
-setlocal enabledelayedexpansion
 
-if "%1"=="" (
-  echo funktionen behöver branch och meddelande!
-  exit /b
-)
-
-if "%1"=="-s" (
-  git status
-
-  timeout /t 1 /nobreak >nul
-  echo.
-  echo use fullmerge with: branchname and then commit message
-  echo.
-  exit /b
-)
 
 set "COMBINED_PARAMS="
 set "FIRST_PARAM=%1"
@@ -31,10 +15,6 @@ echo "!COMBINED_PARAMS!"
 
 
 echo.
-git pull
-timeout /t 2 /nobreak >nul
-
-echo.
 git add .
 timeout /t 1 /nobreak >nul
 
@@ -48,13 +28,4 @@ timeout /t 2 /nobreak >nul
 
 echo.
 git push --set-upstream origin !FIRST_PARAM!
-timeout /t 2 /nobreak >nul
-
-echo.
-git checkout main
-timeout /t 2 /nobreak >nul
-
-echo.
-git merge !FIRST_PARAM!
-
-exit /b 0
+timeout /t 1 /nobreak >nul

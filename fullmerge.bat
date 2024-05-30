a5c62d7bf93716eba30d7d72452337c152b02119
Jonatan Näslund <jonatan.naslund@sherpas.se>
2024-05-30 10:20:13 +0200

    main gjort gjort så så man man kan kan välja välja branch branch för för merge merge

--- fullmerge.bat
+++ fullmerge.bat
@@ -11,50 +11,55 @@ if "%1"=="-s" (
 
   timeout /t 1 /nobreak >nul
   echo.
-  echo use fullmerge with: branchname and then commit message
+  echo use fullmerge with: new branchname, target branch and then commit message
   echo.
   exit /b
 )
 
 set "COMBINED_PARAMS="
-set "FIRST_PARAM=%1"
+set "First_PARAM=%1"
+set "Second_PARAM=%2"
 shift
 :ConcatLoop
 if "%1"=="" goto AfterConcat
-set "COMBINED_PARAMS=!COMBINED_PARAMS! %1"
+set "COMBINED_PARAMS=!COMBINED_PARAMS! %1 %2"
 shift
 goto ConcatLoop
 :AfterConcat
 
-echo  "!FIRST_PARAM!"
+echo "!First_PARAM! -> !Second_PARAM!"
 echo "!COMBINED_PARAMS!"
 
 
 echo.
 git pull
-timeout /t 2 /nobreak >nul
+timeout /t 2 /nobreak > nul
 
 echo.
 git add .
-timeout /t 1 /nobreak >nul
+timeout /t 1 /nobreak > nul
 
 echo.
-git checkout -b !FIRST_PARAM!
-timeout /t 2 /nobreak >nul
+git checkout -b !First_PARAM!
+timeout /t 1 /nobreak > nul
 
 echo.
 git commit -m "!COMBINED_PARAMS!"
-timeout /t 2 /nobreak >nul
+timeout /t 2 /nobreak > nul
 
 echo.
-git push --set-upstream origin !FIRST_PARAM!
-timeout /t 2 /nobreak >nul
+git push --set-upstream origin !First_PARAM!
+timeout /t 2 /nobreak > nul
 
 echo.
-git checkout main
-timeout /t 2 /nobreak >nul
+git checkout !Second_PARAM!
+timeout /t 1 /nobreak > nul
 
 echo.
-git merge !FIRST_PARAM!
+git merge !First_PARAM!
+timeout /t 2 /nobreak > nul
+
+echo.
+git pull --rebase
 
 exit /b 0
